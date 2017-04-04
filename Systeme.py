67df6d4342a4ae5b17c8172d29c2322068d06180
manuelpiquette <manu88@live.ca>
2017-04-04 11:08:09 -0400

    Update Systeme.py

--- Systeme.py
+++ Systeme.py
@@ -16,12 +16,17 @@ class Systeme():
         self.creerplanetes()
         
     def creerplanetes(self):
-        systemeplanetaire=random.randrange(5) # 4 chance sur 5 d'avoir des planetes
-        if systemeplanetaire:
+       
             nbplanetes=random.randrange(12)+1
             for i in range(nbplanetes):
                 type=random.choice(["roc","gaz","glace"])
                 distsol=random.randrange(250)/10 #distance en unite astronomique 150000000km
                 taille=random.randrange(50)/100 # en masse solaire
                 angle=random.randrange(360)
-                self.planetes.append(Planete(self,type,distsol,taille,angle))
+                x=random.randrange(5000)
+                y=random.randrange(5000)
+                for i in self.planetes:
+                    while (x < i.posXatterrissage+100 and x > i.posXatterrissage-100   or  y < i.posYatterrissage+100 and y > i.posYatterrissage-100) :
+                        x=random.randrange(5000)
+                        y=random.randrange(5000)    
+                self.planetes.append(Planete(self,type,distsol,taille,angle,x,y))
